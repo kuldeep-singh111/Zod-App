4b0dc4099125aabc01b21ef64dd6a627a6724d3f
kuldeep singh <129648641+kuldeep-singh111@users.noreply.github.com>
2024-11-28 15:25:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: 'https://melodic-semifreddo-547392.netlify.app',
         credentials: true,
     })
 );
