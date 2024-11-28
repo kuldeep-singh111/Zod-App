3da2f52b952aa9902254ba73319ddeee729a5459
kuldeep singh <129648641+kuldeep-singh111@users.noreply.github.com>
2024-11-28 12:29:40 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectDB = async ()=>{
-    await mongoose.connect(process.env.MONGO_URL)
-    console.log("DB Connected....")
-}  
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,     
+    useUnifiedTopology: true, 
+    ssl: true                  
+  });
+  console.log("DB Connected....");
+};
 
 module.exports = connectDB;
